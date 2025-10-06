b33b821da46e19d043cfe46ad803223b2fb7aefe
Luis Herrera <luigi97@live.com.mx>
2025-10-06 12:07:24 +0200

    Funktion HSV erstellt

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,82 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//Pinbelegung
+#define ROT = 9
+#define GRUEN = 10
+#define BLAU = 11
+#define DRAHTFALLE = 4
+
+//Variablen
+long zaehler = 0;
+int erschuetterung_zaehler = 0;
+byte betriebsart = 1;
+boolean erschuetterung_status;
+float helligkeit = 0.0;
+float zufallsfaktor[4]; //Z0 bis Z3
+
+// Funktionsdeklarationen
+void HSV(int, double, double);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+//Funktionsdefinitionen
+void HSV(int H, double S, double V) {
+  //Hier machen wir uns den HSV-Farbraum zunutze. Der entsprechende Algorithmus ist im Internet zu finden.
+
+  double R = 0, G = 0, B = 0;
+
+  //Dominierende Farbe bestimmen und Hilfswerte bestimmen
+  int h_i = (int) floor(H/60.0);
+
+  double f = H/60.0 - h_i;
+  double p = V * (1-S);
+  double q = V * (1-S*f);
+  double t = V * (1-S*(1-f));
+
+  switch(h_i){  //Hilfswerte werden aufgeteilt. Dominierende Farbe als Kriterium
+
+    case 0: //rot dominiert, Tendenz zu gruen
+      R = V;
+      G = t;
+      B = p;
+      break;
+    case 1: //gruen dominiert ,Tendenz zu rot
+      R = q;
+      G = V;
+      B = p;
+      break;
+      case 2: //gruen dominiert, Tendenz zu blau
+      R = p;
+      G = V;
+      B = t;
+      break;
+    case 3: //blau dominiert ,Tendenz zu gruen
+      R = p;
+      G = q;
+      B = V;
+      break;
+      case 4: //blau dominiert, Tendenz zu rot
+      R = t;
+      G = p;
+      B = V;
+      break;
+    case 5: //rot dominiert ,Tendenz zu blau
+      R = V;
+      G = p;
+      B = q;
+      break;
+  }
+
+  //Wertebereich eingrenzen
+  int rot = constrain ((int)255*R,0,255);
+  int gruen = constrain ((int)255*G,0,255);
+  int blau = constrain ((int)255*B,0,255);
+
+  analogWrite(ROT, rot);
+  analogWrite(GRUEN, gruen);
+  analogWrite(BLAU, blau);
 }
